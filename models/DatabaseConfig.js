4221d34f0ed5916d6a20cc73bf95320fa24128f8
Valentin Tsatskin <val@valtsatskin.com>
2013-02-02 11:40:19 -0500

    syntax fixes for mongoose

--- models/DatabaseConfig.js
+++ models/DatabaseConfig.js
@@ -1,21 +1,21 @@
 var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
-    _id: Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
 	name: 'string',
 	password: 'string',
 	email: 'string'
 	});
 
 var ArtistSchema = mongoose.Schema({
-    _id: Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
 	name: 'string',
 	genre: 'string',
 	topSongs: ['string']
 	});
 
 var LoungeSchema = mongoose.Schema({
-    _id: Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
 	name: 'string',
 	geolocation: {type: ['number'], index:'2d'},
 	user: [UserSchema],
