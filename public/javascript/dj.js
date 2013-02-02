c38f0f1848ba77b74bcaac435e971e398bf427bb
Valentin Tsatskin <val@valtsatskin.com>
2013-02-02 10:35:52 -0500

    some 8AM stuff that doesn't work

--- public/javascript/dj.js
+++ public/javascript/dj.js
@@ -4,4 +4,18 @@ $(function(){
 	$("#add-song").click(function(){
 		
 	});
+
+  var config =  {
+    playlist: {
+      title: 'Random videos',
+      videos: [
+        { id: 'hPzNl6NKAG0', title: 'Maru the cat' }
+      ]
+    }
+  };
+
+
+  var player = $("#player").player(config);
+  console.log(player);
+  player.player('cueVideo', { id: "hNAKWF1uQ08" });
 });
