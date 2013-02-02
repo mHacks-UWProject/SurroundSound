c565083ee91e22bd4a18b0d38d9a0e3359988a77
Adam Wootton <superadamwo@hotmail.com>
2013-02-02 17:49:47 -0500

    added a lot more routes

--- models/DatabaseConfig.js
+++ models/DatabaseConfig.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId
 
-var UserSchema = mongoose.Schema({
-	name: String,
-	password: String,
-	email: String,
-	lounge: LoungeSchema
-	});
 	
 var ArtistSchema = mongoose.Schema({
 	name: String,
@@ -14,22 +9,34 @@ var ArtistSchema = mongoose.Schema({
 	count: Number,
 	likes: Number,
 	dislikes: Number,
-	request: Boolean
+	request: Boolean,
 	});
 
 var LoungeSchema = mongoose.Schema({
 	name: String,
 	geolocation: {type: [Number], index:'2d'},
-	user: [UserSchema],
 	queue: [String],
 	loungePassword: String,
-	artists: [ArtistSchema],
-	requested: [{song:String, artist:String}]
+	artists: [ObjectId],
+	requested: [{song:String, artist:String}],
+	user: ObjectId
 	});
 
+var UserSchema = mongoose.Schema({
+	name: String,
+	password: String,
+	email: String,
+	lounge: ObjectId
+	});
+
+var DeviceSchema = mongoose.Schema({
+	webid: Number,
+	regid: Number,
+})
 
 LoungeSchema.index({geolocation: "2d"});
 
 mongoose.model('User', UserSchema);
 mongoose.model('Artist', ArtistSchema);
 mongoose.model('Lounge', LoungeSchema);
+mongoose.model('Device', DeviceSchema);
