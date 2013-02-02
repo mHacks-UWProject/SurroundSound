fb26d2c9e0491a76fa9ed9694c83bbb63b80e264
Michaela Farova <michaela@mfaro.com>
2013-02-02 05:11:11 -0500

    working on databse functions

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,17 @@
 /*
  * GET home page.
  */
+ 
+var database = require("../models/DatabaseFunctions.js")
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+	res.render('index', { title: 'Express' });
+	};
+
+exports.postArtists = function(req, res){
+	database.importData(req.body);
+	};
+	
+exports.Queue = function(req, res){
+	
+	};
