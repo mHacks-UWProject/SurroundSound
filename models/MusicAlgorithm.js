20140144597707ff1df844bfc0951b9c58cf2634
Michaela Farova <michaela@mfaro.com>
2013-02-02 21:45:13 -0500

    getNextSong implemented add requested song functionality

--- models/MusicAlgorithm.js
+++ models/MusicAlgorithm.js
@@ -1,21 +1,25 @@
 var mongoose = require('mongoose');
 var RAND_MAX = 5;
 
-exports.getNextSong = function(){
+exports.getNextSong = function(loungeId){
+	var Lounge = mongoose.model('Lounge');
+	var lounge = Lounge.find({ _id: loungeId });
+	
 	// get random number
 	var randNum = Math.floor(Math.random()*RAND_MAX);
 	
 	//if selecting requested item or not
 	if(randNum == 0){
-		/*
-		var Artist = mongoose.model('Artist');
-		Lounge.findOneAndUpdate({ request: true }, function (err, lounge) {
-			if (!err) {
-				doc.request = false;
-				doc.save(callback);
-				return doc;
-			}
-		});*/
+		var requested = lounge.requested;
+		var nextSong = requested[0];
+		var newRequested = [];
+		
+		for(var i = 0; i < requested.count()-1; i++) {
+			newRequested[i] = requested[i+1]
+		}
+		
+		lounge.requested = newRequested;
+		lounge.save();
 	} 
 	else {
 		
