6156cd589549ba535a335ed811356aace0c3fbb6
Michaela Farova <michaela@mfaro.com>
2013-02-03 01:27:48 -0500

    Removing unused functions

--- models/MusicAlgorithm.js
+++ models/MusicAlgorithm.js
@@ -30,14 +30,3 @@ exports.getNextSong = function(loungeId){
 		
 	});
 }
-
-
-function compare(a,b) {
-  if (a.last_nom < b.last_nom)
-     return -1;
-  if (a.last_nom > b.last_nom)
-    return 1;
-  return 0;
-}
-
-
