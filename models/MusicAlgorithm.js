44b7eb4e62ea431f442a352d84036fcd729ccdc9
Michaela Farova <michaela@mfaro.com>
2013-02-02 23:20:16 -0500

    Vics commit of michaelas stuffs

--- models/MusicAlgorithm.js
+++ models/MusicAlgorithm.js
@@ -4,6 +4,7 @@ var RAND_MAX = 5;
 exports.getNextSong = function(loungeId){
 	var Lounge = mongoose.model('Lounge');
 	var lounge = Lounge.find({ _id: loungeId });
+	var nextSong;
 	
 	// get random number
 	var randNum = Math.floor(Math.random()*RAND_MAX);
@@ -11,20 +12,50 @@ exports.getNextSong = function(loungeId){
 	//if selecting requested item or not
 	if(randNum == 0){
 		var requested = lounge.requested;
-		var nextSong = requested[0];
-		var newRequested = [];
 		
-		for(var i = 0; i < requested.count()-1; i++) {
-			newRequested[i] = requested[i+1]
+		if(typeof requested != 'undefined' && requested.length() != 0){
+			var newRequested = [];
+			
+			nextSong = requested[0];
+			
+			for(var i = 0; i < requested.count()-1; i++) {
+				newRequested[i] = requested[i+1]
+			}
+			
+			lounge.requested = newRequested;
+			lounge.save();
 		}
-		
-		lounge.requested = newRequested;
-		lounge.save();
 	} 
-	else {
+	
+	// get non requested song based on algorithm
+	
+	var artists = lounge.artists;
+	artists.sort(function(a,b) {
+		if (a.count < b.count)
+			return -1;
+		if (a.count > b.count)
+			return 1;
+		return 0;
+	});
+	
+	for(var i = 0; i < artists.count()/2; i++){
+		
 		
 		
 		
 	}
+	
+	
+	
 }
+
+
+function compare(a,b) {
+  if (a.last_nom < b.last_nom)
+     return -1;
+  if (a.last_nom > b.last_nom)
+    return 1;
+  return 0;
+}
+
 
