76bdf053cb3742d23b7f30c6a823834449ccfe32
Michaela Farova <michaela@mfaro.com>
2013-02-03 01:55:19 -0500

    Ensured that there are lounges before proceeding with algorithm

--- models/MusicAlgorithm.js
+++ models/MusicAlgorithm.js
@@ -4,40 +4,34 @@ var RAND_MAX = 5;
 exports.getNextSong = function(loungeId){
 	var Lounge = mongoose.model('Lounge');
 	Lounge.findById(loungeId, function(err, lounge) {
-		var nextSong;
-		
-		// get random number
-		var randNum = Math.floor(Math.random()*RAND_MAX);
-		
-		//if selecting requested item or not
-		if(randNum == 0 && lounge.requested.length > 0 ){
-			nextSong = lounge.requested.shift();
-		} else {
-			// get non requested song based on algorithm
-			var artists = lounge.artists;
-			/*
-			artists.sort(function(a,b) {
-				if (a.count < b.count)
-					return -1;mm
-				if (a.count > b.count)
-					return 1;
-				return 0;
-			});*/
-			var artistScore = [];
-			for(var i = 0; i < artists.length; i++){
-				var artist = artists[i]
-				artistScore.push(artist.count + artist.likes - artist.dislikes)
-			}
-			var highest = 0;
-			var highestindex = 0;
-			for (var i = 0; i < artistScore.length; i ++ ) {
-				if (artistScore[i] > highest){
-					highest = artistScore[i];
-					highestindex = i;
+		if(!err){
+			var nextSong;
+			
+			// get random number
+			var randNum = Math.floor(Math.random()*RAND_MAX);
+			
+			//if selecting requested item or not
+			if(randNum == 0 && lounge.requested.length > 0 ){
+				nextSong = lounge.requested.shift();
+			} else {
+				// get non requested song based on algorithm
+				var artists = lounge.artists;
+				var artistScore = [];
+				for(var i = 0; i < artists.length; i++){
+					var artist = artists[i]
+					artistScore.push(artist.count + artist.likes - artist.dislikes)
 				}
+				var highest = 0;
+				var highestindex = 0;
+				for (var i = 0; i < artistScore.length; i ++ ) {
+					if (artistScore[i] > highest){
+						highest = artistScore[i];
+						highestindex = i;
+					}
+				}
+				var highArtist = lounge.artists[highest]
+				nextSong = {artist: highArtist, song: lounge.artists[highest].topSongs[Math.random() * lounge.artists[highest].length]};
 			}
-			var highArtist = lounge.artists[highest]
-			nextSong = {artist: highArtist, song: lounge.artists[highest].topSongs[Math.random() * lounge.artists[highest].length]};
 		}
 		
 	});
